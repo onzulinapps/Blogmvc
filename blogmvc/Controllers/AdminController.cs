9f2b5648806f79ed97c4efdaa8ecb83416d0e8de
techcworld <techcworld@outlook.com>
2016-08-10 12:36:58 +0200

    el problema lo tengo ahora en el formulario que no se como hacerlo

---
+++ blogmvc/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using blogmvc.Models;
+
+namespace blogmvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET UserProfile
+        public ActionResult UserProfile()
+        {
+            //comprobar datos que se muestran en el formulario
+
+            return View("User", db.Option.ToList());
+
+
+        }
+        public ActionResult UserView()
+        {
+            return View("User");
+        }
+        /*
+        public ActionResult UserSend ()
+        {
+
+        }
+        */
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditOption()
+        {
+            Option Option = new Option();
+            List<Option> Options = db.Option.ToList();
+            //return View(Options);
+            /*
+            int count = Options.Count();
+            for (int i = 0; i < count; i++ )
+            {
+                Options.ElementAt(i);
+            }*/
+            * /*
+            if (ModelState.IsValid)
+            {
+                foreach (string OptionName in Options)
+                {
+
+                } 
+                
+            }
+            */
+            return View("User", Options);
+        }
+
+    }
+}
